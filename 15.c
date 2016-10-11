c5eddb8992681c86a5ed45af4ddf0724796f3c75
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-10-11 08:57:04 -0700

    string

---
+++ 15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    printf("Display the Upper case: ");
+    printf("%c\n",toupper(ch));
+    //printf("%c",tolower(ch));
+
+    return 0;
+}
