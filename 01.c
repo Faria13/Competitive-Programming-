c53494ee2ca9f062e80a4032512fc486dbd6c3fc
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-10-11 01:02:31 -0700

    Print N numbers

---
+++ 01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int N,i;
+
+    while(scanf("%d",&N) == 1){
+    for(i = 1; i <= N; i++){
+        printf("%d ",i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
